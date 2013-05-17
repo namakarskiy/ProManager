9b82c973b31859b7ca41743a77863ed0153a7c69
Andrew <anamakarskiy@gmail.com>
2013-05-17 03:43:00 +0300

    test commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,20 +9,20 @@ MainWindow::MainWindow(QWidget *parent) :
     header<<"pid"<<"name"<<"state"<<"ppid"<<"nice"<<"size";//<<"ram"<<"swap"<<"ololo";
     ui->setupUi(this);
     ui->treeWidget->setColumnCount(6);
-    ui->treeWidget->setHeaderLabels(header);bergi890
-            ewdAEbergi890
+    ui->treeWidget->setHeaderLabels(header);
+
 
     ui->treeWidget->setColumnWidth(0,50);
     ui->treeWidget->setColumnWidth(1,200);
     ui->treeWidget->setSortingEnabled(true);
     QTreeWidgetItem *item=0;
-    /*for(int i=0;i<10;i++)
+    for(int i=0;i<10;i++)
     {
         item = new QTreeWidgetItem(ui->treeWidget);
         item->setText(0,QString::number(i));
         item->setText(1,"qwed"+QString::number(i));
         ui->treeWidget->setItemExpanded(item,true);
-    }*/
+    }
     //ui->treeWidget->s;
 }
 
